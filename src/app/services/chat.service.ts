1a5c654ff5c26991a059820527982abd0fa3ef77
MadaZZ <sparshranaazz@gmail.com>
2018-03-09 22:56:44 +0530

    Can't remove bugs due to version differences

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-//import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFireDatabase,FirebaseListObservable } from 'angularfire2/database-deprecated';
+//import {  } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable,FirebaseListObservable } from 'angularfire2/database-deprecated';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../services/auth.service';
